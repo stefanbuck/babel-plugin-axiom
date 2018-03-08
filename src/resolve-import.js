c2f9cc98d697f04f06de167a26a7e6eb2f06c07c
Stefan Buck <stefanb@brandwatch.com>
2018-03-08 11:41:29 +0100

    Tweak plugin to handle axiom packages

--- src/resolve-import.js
+++ src/resolve-import.js
@@ -3,22 +3,44 @@ import fs from 'fs';
 import Module from 'module';
 import { parse } from 'babylon';
 
-const _axiomPath = path.dirname(
-  Module._resolveFilename('bw-axiom', Object.assign({}, new Module, {
-    'paths': Module._nodeModulePaths(process.cwd()),
-  }))
-);
+const supportedAxiomPackages = [
+  '@brandwatch/axiom-automation-testing',
+  '@brandwatch/axiom-charts',
+  '@brandwatch/axiom-components',
+  '@brandwatch/axiom-composites',
+  '@brandwatch/axiom-formatting',
+  '@brandwatch/axiom-localization',
+  '@brandwatch/axiom-materials',
+  '@brandwatch/axiom-utils',
+];
 
-const axiomPath = _axiomPath.slice(0, _axiomPath.lastIndexOf('bw-axiom') + 'bw-axiom'.length);
-const indexFile = fs.readFileSync(`${axiomPath}/src/index.js`, 'utf8');
-const exportsList = parse(indexFile, { sourceType: 'module' })
-  .program
-  .body
-  .reduce((exprts, exprt) => Object.assign({}, exprts,
-    exprt.specifiers.reduce((specs, spec) => Object.assign({}, specs, {
-      [spec.exported.name]: path.join('bw-axiom/lib', exprt.source.value),
-    }), {}),
-  ), {});
+const pkg = require(path.join(process.cwd(), 'package.json')); // TODO this is not very solid
+
+const usedAxiomPackages = Object.keys(pkg.dependencies)
+  .filter(dep => supportedAxiomPackages.includes(dep));
+
+function load(memo, packageName) {
+  const _axiomPath = path.dirname(
+    Module._resolveFilename(packageName, Object.assign({}, new Module, {
+      'paths': Module._nodeModulePaths(process.cwd()),
+    }))
+  );
+  const axiomPath = path.join(_axiomPath, '../src');
+  const indexFile = fs.readFileSync(`${axiomPath}/index.js`, 'utf8');
+
+  Object.assign(memo, parse(indexFile, { sourceType: 'module' })
+    .program
+    .body
+    .reduce((exprts, exprt) => Object.assign({}, exprts,
+      exprt.specifiers.reduce((specs, spec) => Object.assign({}, specs, {
+        [spec.exported.name]: path.join(`${packageName}/dist`, exprt.source.value),
+      }), {}),
+    ), {}));
+
+  return memo;
+}
+
+const exportsList = usedAxiomPackages.reduce(load, {});
 
 export default (name) => {
   if (exportsList[name]) {
